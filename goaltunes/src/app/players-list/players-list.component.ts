1d0c6171a5cae342a30eb43f71dee1675bc5a94f
Lars van der Sluijs <lars.van.der.sluijs@hva.nl>
2023-10-25 18:13:00 +0200

    update

--- goaltunes/src/app/players-list/players-list.component.ts
+++ goaltunes/src/app/players-list/players-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
+interface Player {
+  name: string;
+  audio: string;
+}
 @Component({
   selector: 'app-players-list',
   templateUrl: './players-list.component.html',
   styleUrls: ['./players-list.component.scss']
 })
-export class PlayersListComponent {
+export class PlayersListComponent implements OnInit{
+  players: Player[] = [];
+  ngOnInit() {
+    this.players = JSON.parse(localStorage.getItem('players') || '[]');
+  }
 
+  deletePlayer(index: number) {
+    // Remove the player from the array based on the index
+    this.players.splice(index, 1);
+
+    // Update the players in localStorage
+    localStorage.setItem('players', JSON.stringify(this.players));
+  }
 }
