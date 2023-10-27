30cab5a13b35b8eaf33a137a32b6656ff2ff17db
Lars Stage <lars.van.der.sluijs@hva.nl>
2023-10-27 09:27:35 +0200

    stop button appears correct

--- goaltunes/src/app/play-screen/play-screen.component.ts
+++ goaltunes/src/app/play-screen/play-screen.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 
 interface Player {
   name: string;
@@ -11,15 +11,18 @@ interface Player {
   templateUrl: './play-screen.component.html',
   styleUrls: ['./play-screen.component.scss']
 })
-export class PlayScreenComponent implements OnInit{
-    players: Player[] = [];
-    ngOnInit() {
-      this.fetchPlayersFromLocalstorage();
-    }
+export class PlayScreenComponent implements OnInit {
+  players: Player[] = [];
+  audio: HTMLAudioElement | null = null;
+  currentAudioIndex: number | null = null; // Added this property to track the index of the currently playing audio
+
   constructor(public dialog: MatDialog) {}
 
-  fetchPlayersFromLocalstorage() {
-    // Haal de array van spelers op uit localStorage
+  ngOnInit(): void {
+    this.fetchPlayersFromLocalstorage();
+  }
+
+  fetchPlayersFromLocalstorage(): void {
     this.players = JSON.parse(localStorage.getItem('players') || '[]');
   }
 
@@ -32,17 +35,30 @@ export class PlayScreenComponent implements OnInit{
     }
 
     const byteArray = new Uint8Array(byteNumbers);
-    const blob = new Blob([byteArray], { type: 'audio/mp3' }); // adjust the mime type accordingly
+    const blob = new Blob([byteArray], {type: 'audio/mp3'});
     return URL.createObjectURL(blob);
   }
 
+  playAudio(base64Data: string, index: number): void { // Updated method signature to include index
+    this.stopAudio();
 
-  playAudio(base64Data: string) {
     const audioSrc = this.convertBase64ToAudioObjectURL(base64Data);
-    const audio = new Audio(audioSrc);
-    audio.play();
+    this.audio = new Audio(audioSrc);
+    this.audio.play();
+    this.currentAudioIndex = index; // Store the index of the currently playing audio
+
+    // Listen for when the audio ends
+    this.audio.addEventListener('ended', () => {
+      this.currentAudioIndex = null; // Reset the index
+    });
   }
 
-
-
+  stopAudio(): void {
+    if (this.audio) {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.audio = null;
+      this.currentAudioIndex = null; // Reset the index when the audio is stopped
+    }
+  }
 }
